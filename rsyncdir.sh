68bb667c5a9945648001ea3607d3ebbbf0f6bc3c
andyceo <andyceo@yandex.ru>
2017-12-03 01:24:07 +0300

    Simplify rsyncdir.sh script.

--- rsyncdir.sh
+++ rsyncdir.sh
@@ -2,25 +2,12 @@
 
 set -e
 
-SOURCE=${SOURCE:-}
-DESTINATION=${DESTINATION:-}
+RSYNCDIR_SOURCE=${RSYNCDIR_SOURCE:-}
+RSYNCDIR_DESTINATION=${RSYNCDIR_DESTINATION:-}
 
 echo
-echo "Directory backup (from $SOURCE to $DESTINATION) started at `date --utc --iso-8601=seconds`"
-
-if [[ -z "${SOURCE}" ]]; then
-    echo "Source directory not set! Exiting..."
-    exit 1
-fi
-
-if [[ -z "${DESTINATION}" ]]; then
-    echo "Destination directory not set! Exiting..."
-    exit 2
-fi
-
-rsync -ahvz --stats --delete-after ${SOURCE} ${DESTINATION}
-
-echo "Directory backups (from $SOURCE to $DESTINATION) ended at `date --utc --iso-8601=seconds`"
-echo
-
+echo "Directory backup (from $RSYNCDIR_SOURCE to $RSYNCDIR_DESTINATION) started at `date --utc --iso-8601=seconds`"
+rsync -ahvz --stats --delete-after ${RSYNCDIR_SOURCE} ${RSYNCDIR_DESTINATION}
 # @todo: logic for trying rsync until no files are changed during rsync
+echo "Directory backups (from $RSYNCDIR_SOURCE to $RSYNCDIR_DESTINATION) ended at `date --utc --iso-8601=seconds`"
+echo
