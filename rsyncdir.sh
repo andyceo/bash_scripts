d82d575a31a46840d185c2ae2f9dd8651e92abc2
andyceo <andyceo@yandex.ru>
2017-12-06 17:05:58 +0300

    rsyncdir.sh: Now source and destination are given from script arguments (as for originally rsync), and if they are not given, read respective environment variables and if they not set too, leave them undefined

--- rsyncdir.sh
+++ rsyncdir.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-RSYNCDIR_SOURCE=${RSYNCDIR_SOURCE:-}
-RSYNCDIR_DESTINATION=${RSYNCDIR_DESTINATION:-}
+RSYNCDIR_SOURCE=${1:-${RSYNCDIR_SOURCE:-}}
+RSYNCDIR_DESTINATION=${2:-${RSYNCDIR_DESTINATION:-}}
 
 echo
 echo "Directory backup (from $RSYNCDIR_SOURCE to $RSYNCDIR_DESTINATION) started at `date --utc --iso-8601=seconds`"
 rsync -ahvz --stats --delete-after ${RSYNCDIR_SOURCE} ${RSYNCDIR_DESTINATION}
-# @todo: logic for trying rsync until no files are changed during rsync
+# @todo: add optional logic for trying rsync until no files are changed during rsync
 echo "Directory backups (from $RSYNCDIR_SOURCE to $RSYNCDIR_DESTINATION) ended at `date --utc --iso-8601=seconds`"
 echo
