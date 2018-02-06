64db18b4e12b9c4076540bd74c2341beecc96828
andyceo <andyceo@yandex.ru>
2018-02-06 13:08:01 +0300

    Update shellcheck script to return error when error occured

--- shellcheck.sh
+++ shellcheck.sh
@@ -2,5 +2,5 @@
 
 apt install shellcheck
 
-find . -type f -name "*.sh" -exec bash -n {} \;
-find . -type f -name "*.sh" -exec shellcheck {} \;
+find . -type f -name "*.sh" -print0 | xargs -n 1 -0 bash -n
+find . -type f -name "*.sh" -print0 | xargs -n 1 -0 shellcheck
