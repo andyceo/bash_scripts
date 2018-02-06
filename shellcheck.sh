3fe9df2d0cd601289e61f9592e740b8a5d70f2ad
andyceo <andyceo@yandex.ru>
2018-02-06 13:15:30 +0300

    Leave only shellcheck in shellcheck.sh script and add some comments to it

--- shellcheck.sh
+++ shellcheck.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-apt install shellcheck
+# This script use shellcheck to verify shell scripts. On Ubuntu, you can install it with:
+#
+#   apt install shellcheck
+#
+# Also, if you are sure that your script is bash script (not sh, zsh etc), you can use for syntax check:
+#
+#   find . -type f -name "*.sh" -print0 | xargs -n 1 -0 bash -n
 
-find . -type f -name "*.sh" -print0 | xargs -n 1 -0 bash -n
 find . -type f -name "*.sh" -print0 | xargs -n 1 -0 shellcheck
