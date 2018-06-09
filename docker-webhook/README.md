e2b0cd1deccc674669ec82632111ba39c3026098
andyceo <andyceo@yandex.ru>
2018-06-09 19:07:41 +0300

    Update nc-exec.sh: use shell switch-case, prepare to add new commands, start README.md

---
+++ docker-webhook/README.md
@@ -0,0 +1,10 @@
+## Message syntax
+
+    COMMAND ARG_1 [ARG_2 ... [ARG_N]]
+
+## Supported commands
+
+- run TAG
+- service-update SERVICE_NAME [IMAGE_AND_TAG]
+
+    If IMAGE_AND_TAG is not specified, used current one.
--- docker-webhook/nc-exec.sh
+++ docker-webhook/nc-exec.sh
@@ -15,43 +15,65 @@
 #
 # Run this on server from which execute this:
 #
-#     $ echo run v2.14.0-RC1 | nc -q 0 10.1.20.115 54321
+#     $ echo run v2.14.0-RC1 | nc -q 0 10.1.20.115 8080
 #
 ##################################################################################################################
 
+PORT=8080
+STEP=1
+
+echo "Script started!"
 while true ; do
 
-  input=$(nc -l -q 0 -p 54321)
+  echo "Step ${STEP}: listening..."
 
-  echo "Received: $input"
+  input=$(nc -l -q 0 -p ${PORT})
 
-  command=${input%% *}
-  tag=${input##* }
+  command=$(echo ${input} | cut -d' ' -f1)
+  arg1=$(echo ${input} | cut -d' ' -f2)
+  arg2=$(echo ${input} | cut -d' ' -f3)
+  arg3=$(echo ${input} | cut -d' ' -f4)
 
-  if [ "$command" = "run" ] && [ -n "$tag" ]
-  then
+  echo
+  echo "Received: ${input}, parsed as:"
+  echo "Command: ${command}"
+  echo "  ARG_1: ${arg1}"
+  echo "  ARG_2: ${arg2}"
+  echo "  ARG_3: ${arg3}"
 
-    echo "Re-run containers version $tag :"
+  echo
 
-    CONTAINERS="test1.example.com test2.example.com"
-    for name in $CONTAINERS
-    do
-      if docker top $name &>/dev/null
-      then
-        docker rm -f $name
-      fi
-    done
+  case "${command}" in
+    "run")
+      tag=arg1
 
-    PORT_PREFIX=2001 && docker run -d -p `echo $PORT_PREFIX+80|bc`:80 --name test1.example.com -h test1.example.com --restart always --link redis-master --link redis-slave --link memcached -e APP_ENV=dev -e APP_HOST=test1.example.com build.example.com:5000/example.com/app:$tag
+      echo "Re-run containers version $tag :"
 
-    PORT_PREFIX=2002 && docker run -d -p `echo $PORT_PREFIX+80|bc`:80 --name test2.example.com -h test2.example.com --restart always --link redis-master --link redis-slave --link memcached -e APP_ENV=stage -e APP_HOST=test2.example.com build.example.com:5000/example.com/app:$tag
+      CONTAINERS="test1.example.com test2.example.com"
+      for name in $CONTAINERS
+      do
+        if docker top $name &>/dev/null
+        then
+          docker rm -f $name
+        fi
+      done
 
-    docker images --format "table {{.ID}}\t{{.Repository}}\t{{.Tag}}" | grep "example.com/app" | grep -v $tag | cut -d" " -f1 | xargs docker rmi
+      PORT_PREFIX=2001 && docker run -d -p `echo $PORT_PREFIX+80|bc`:80 --name test1.example.com -h test1.example.com --restart always --link redis-master --link redis-slave --link memcached -e APP_ENV=dev -e APP_HOST=test1.example.com build.example.com:5000/example.com/app:$tag
 
-  else
+      PORT_PREFIX=2002 && docker run -d -p `echo $PORT_PREFIX+80|bc`:80 --name test2.example.com -h test2.example.com --restart always --link redis-master --link redis-slave --link memcached -e APP_ENV=stage -e APP_HOST=test2.example.com build.example.com:5000/example.com/app:$tag
 
-    echo "Unknown command: $command, or tag: $tag"
+      docker images --format "table {{.ID}}\t{{.Repository}}\t{{.Tag}}" | grep "example.com/app" | grep -v $tag | cut -d" " -f1 | xargs docker rmi
 
-  fi
+      ;;
+
+    "service-update")
+      echo "SErvice-update: construction in progress..."
+      ;;
+    *)
+      echo "Unknown command: ${command}, or tag: ${tag}, nothing to do"
+      ;;
+  esac
+
+  STEP=$(expr ${STEP} + 1)
 
 done
